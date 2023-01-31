77a5f3ebf7945a5cc214b6dbe54f014c76f22dc3
Kevin Guanyuan Wang <kevin.wgy@gmail.com>
2023-01-30 20:40:30 -0500

    Working on a customizable neighbor communicator

--- GhostFluidOperator.h
+++ GhostFluidOperator.h
@@ -5,6 +5,8 @@
 #include<GhostPoint.h>
 #include<EmbeddedBoundaryFormula.h>
 
+class EmbeddedBoundaryDataSet;
+
 /**************************************************************
 * Class GhostFluidOperator is responsible for populating ghost
 * nodes in ``inactive'' regions next to an embedded boundary.
@@ -15,9 +17,25 @@
 
 class GhostFluidOperator {
 
- 
+  MPI_Comm &comm;
+  
+
+  std::vector<std::pair<Int3, EmbeddedBoundaryFormula> > ghostNodes1; //ghost nodes whose image is inside this subdomain
+
+  std::vector<std::vector<Int3> > ghostNodes2; //ghost nodes whose image is inside another subdomain
+  std::vector<int> ghostNodes2_sender;
+
+  std::vector<std::vector<std::pair<Int3, EmbeddedBoundaryFormula> > > friendsGhostNodes;
+  std::vector<int> friendsGhostNodes_receiver;
+
+  // laser radiance at ghost nodes
+  //   std::vector<double> l1; //ghostNodes1
+  //     std::vector<std::vector<double> > l2; //ghostNodes2
 
 
+public:
+
+  GhostFluidOperator(
 
 
 
---
+++ NeighborCommunicator.h
@@ -0,0 +1,59 @@
+#ifndef _NEIGHBOR_COMMUNICATOR_H_
+#define _NEIGHBOR_COMMUNICATOR_H_
+
+#include<mpi.h>
+#include<Vector3D.h>
+
+/*******************************************************
+ * Class CustomCommunicator sets up a communication
+ * channel between adjacent subdomains. It allows them
+ * to exchange data of arbitrary size
+ * Note: The data type is assumed to be "double". Integers
+ * will be represented as doubles.
+ ******************************************************/
+
+class NeighborCommunicator {
+
+  //! MPI info
+  MPI_Comm& comm;
+  int rank;
+  int size;
+
+
+  //! A nested structure that stores the package to/from a neighbor. Everything except data is fixed.
+  struct Package {
+    enum Type {SEND = 0, RECEIVE = 1} type;
+    int rank; //rank of the package sender/receiver
+    std::vector<double> buffer;
+    Package(Type type_, int rank_ = -1) : type(type_), rank(rank_) {}
+    ~Package() {}
+  };
+
+  std::vector<Package> send_pack;
+  std::vector<Package> recv_pack;
+
+public:
+
+  //! Create the communicator. ghost_nodes: list of ghost nodes that participate in the communication
+  CustomCommunicator(MPI_Comm& comm_, SpaceVariable3D &V, std::vector<Int3> &ghost_nodes);
+
+  //! Create a new communicator that is the same as the one given, except different "dof" and "ghost_width"
+  //! For safety, it is required that the new ghost_width is equal to or larger than that of the given comm.
+  CustomCommunicator(const CustomCommunicator &cc, int dof_, int ghost_width_);
+
+  ~CustomCommunicator();
+
+  //! Data exchange (Passing "v" reduces the cost by <20% (i.e. not much) compared to passing "V")
+  void ExchangeAndInsert(SpaceVariable3D &V);
+  void ExchangeAndInsert(double*** v); //note: this function does not verify compatibility
+
+  //! Can easily add other types of communication (e.g., add, max, etc.)
+
+  //! Get info
+  int NumDOF() {return dof;}
+  int NumGhostLayers() {return ghost_width;}
+
+};
+
+
+#endif
