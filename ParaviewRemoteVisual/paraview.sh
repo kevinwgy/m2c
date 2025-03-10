84fbf41b9b75fd4f12c37754d0519d235adcdc59
Kevin Guanyuan Wang <kevin.wgy@gmail.com>
2025-03-10 16:21:44 -0400

    Updated paraview visualizaiton script & doc (Tinkercliffs updated)

--- ParaviewRemoteVisual/paraview.sh
+++ ParaviewRemoteVisual/paraview.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH -t 3:00:00
-###SBATCH -p normal_q
-#SBATCH -p dev_q
-#SBATCH -A wang_aoe_lab 
+#SBATCH --ntasks-per-node=8
+###SBATCH --ntasks-per-node=32
+#SBATCH -t 4:00:00
+#SBATCH -p normal_q
+###SBATCH -p dev_q
+#SBATCH -A m2clab 
+###SBATCH -A wang_aoe_lab 
 
 # Add any modules you might require. 
-module load ParaView/5.9.1-foss-2021a-mpi
+module load ParaView/5.11.2-foss-2023a 
 cd $SLURM_SUBMIT_DIR
-mpirun -n 32 pvserver
+mpirun -n 8 pvserver
+#mpirun -n 32 pvserver
 exit;
